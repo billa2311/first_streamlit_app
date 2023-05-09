fb1a84f11a6f3a29aebe0d432041918c0bc96fdf
billa2311 <132910187+billa2311@users.noreply.github.com>
2023-05-09 11:54:44 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit
-streamlit.title("my first hero")
-streamlit.header("ALLU")
-streamlit.text("badri")
-streamlit.text("pushpa")
+streamlit.title("My Parents New Healthy Dinner ")
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
